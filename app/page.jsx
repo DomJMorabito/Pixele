6fb639a7e860a8b97a8ebd6e1033646a89e932b3
DomJMorabito <domjmorabito@gmail.com>
2024-09-23 15:24:24 -0400

    Another test

--- app/page.jsx
+++ app/page.jsx
@@ -3,14 +3,10 @@
 import GameCardGrid from "./components/gamecardgrid/GameCardGrid";
 import Footer from "./components/footer/Footer";
 
-// CSS Imports:
-
-import styles from './HomeScreen.module.css';
-
 export default function HomeScreen() {
     return (
         <>
-            <main className = { styles.main }>
+            <main>
                 <GameCardGrid />
             </main>
             <Footer />
