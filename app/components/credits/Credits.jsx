94d216019f354f2884a60119c3164e5309030af4
DomJMorabito <domjmorabito@gmail.com>
2024-09-24 23:34:15 -0400

    Implementation of credits modal to homescreen.

---
+++ app/components/credits/Credits.jsx
@@ -0,0 +1,28 @@
+"use client";
+
+// React Imports:
+
+import React from 'react';
+
+// CSS Imports:
+
+import "./Credits.css";
+
+export default function Credits({ isVisible, onClose }) {
+    return (
+        <div id = "credits-modal" className = {isVisible ? 'show' : 'hide'}>
+            <div id = "credits-content">
+                <span id = "close-button" onClick = {onClose}>&times;</span>
+                    <p id = "header">Credits & Attributions</p>
+                    <p id = "top-line">All rights reserved to the following studios/publishers:</p>
+                    <p id = "line">
+                        © Blizzard Entertainment, Inc. / © Nintendo / © Riot Games, Inc / © Ubisoft Entertainment /
+                        © Mojang AB / © Supercell Oy / © PopCap Games, Inc. / © Electronic Arts Inc. / HAL Laboratory, Inc. /
+                        Pokémon. / Creatures Inc. / GAME FREAK Inc. / SHIGESATO ITOI / APE inc. / INTELLIGENT SYSTEMS /
+                        Konami Digital Entertainment / SEGA / CAPCOM CO., LTD. / BANDAI NAMCO Entertainment Inc. /
+                        MONOLITHSOFT / CAPCOM U.S.A., INC. / SQUARE ENIX CO., LTD. / ATLUS / Microsoft / SNK CORPORATION. / Disney\
+                    </p>
+            </div>
+        </div>
+    );
+}
--- app/components/footer/Footer.jsx
+++ app/components/footer/Footer.jsx
@@ -1,51 +1,83 @@
+"use client";
+
+// Next.js Imports:
+
 import Image from 'next/image';
+
+// React Imports:
+
+import React, { useState } from 'react';
+
+// Component Imports:
+
+import Credits from '../credits/Credits';
+
+// CSS Imports:
+
 import './Footer.css';
 
 export default function Footer() {
+
+    const [isVisible, setVisible] = useState(false);
+
+    const show = () => {
+        setVisible(true);
+    }
+
+    const hide = () => {
+        setVisible(false)
+    }
     return (
-        <footer>
-            <div id="footer-content">
-                <div id="social-container">
-                    <div id="ko-fi-container">
-                        <a href="https://ko-fi.com/pixele" id="ko-fi-button" target="_blank" rel="noopener noreferrer">
-                            <Image 
-                                src="/footer/ko-fi.png" 
-                                alt="Ko-fi" 
-                                width={100} 
-                                height={100} 
-                            />
-                            Support me!
-                        </a>
+        <>
+            <footer>
+                <div id="footer-content">
+                    <div id="social-container">
+                        <div id="ko-fi-container">
+                            <a href="https://ko-fi.com/pixele" id="ko-fi-button" target="_blank"
+                               rel="noopener noreferrer">
+                                <Image
+                                    src="/footer/ko-fi.png"
+                                    alt="Ko-fi"
+                                    width={100}
+                                    height={100}
+                                />
+                                Support me!
+                            </a>
+                        </div>
+                        <div id="social-links">
+                            <a href="https://twitter.com/blockspinner44" className="social-icon" target="_blank"
+                               rel="noopener noreferrer">
+                                <Image
+                                    src="/footer/twitter.png"
+                                    alt="Twitter"
+                                    width={100}
+                                    height={100}
+                                />
+                            </a>
+                            <a href="https://www.reddit.com/r/Pixelegg/" className="social-icon" target="_blank"
+                               rel="noopener noreferrer">
+                                <Image
+                                    src="/footer/reddit.png"
+                                    alt="Reddit"
+                                    width={100}
+                                    height={100}
+                                />
+                            </a>
+                            <a href="https://discord.gg/WG6ySKvZJP" className="social-icon" target="_blank"
+                               rel="noopener noreferrer">
+                                <Image
+                                    src="/footer/discord.png"
+                                    alt="Discord"
+                                    width={100}
+                                    height={100}
+                                />
+                            </a>
+                        </div>
+                        <p id="credits" onClick = {show}>Credits & Attributions</p>
                     </div>
-                    <div id="social-links">
-                        <a href="https://twitter.com/blockspinner44" className="social-icon" target="_blank" rel="noopener noreferrer">
-                            <Image 
-                                src="/footer/twitter.png" 
-                                alt="Twitter" 
-                                width={100} 
-                                height={100}
-                            />
-                        </a>
-                        <a href="https://www.reddit.com/r/Pixelegg/" className="social-icon" target="_blank" rel="noopener noreferrer">
-                            <Image 
-                                src="/footer/reddit.png" 
-                                alt="Reddit" 
-                                width={100} 
-                                height={100} 
-                            />
-                        </a>
-                        <a href="https://discord.gg/WG6ySKvZJP" className="social-icon" target="_blank" rel="noopener noreferrer">
-                            <Image 
-                                src="/footer/discord.png" 
-                                alt="Discord" 
-                                width={100} 
-                                height={100} 
-                            />
-                        </a>
-                    </div>
-                    <p id="credits">Credits & Attributions</p>
                 </div>
-            </div>
-        </footer>
+            </footer>
+            <Credits isVisible = {isVisible} onClose = {hide} />
+        </>
     );
 }
