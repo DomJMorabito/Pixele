b00117bcdb0dbbf31a0c7d70cc095d50bc9bdcb3
DomJMorabito <domjmorabito@gmail.com>
2024-09-29 12:59:22 -0400

    Start to styling for register page.

--- app/components/register/RegisterModal.jsx
+++ app/components/register/RegisterModal.jsx
@@ -7,17 +7,16 @@ import "./RegisterModal.css";
 export default function RegisterModal() {
     return (
         <>
-            <div id = "error-indicator"></div>
             <div id = "register-container">
                 <div id = "register-box">
                     <p id = "welcome">Join the Fight!</p>
                     <form id = "registration-form">
                         <p id = "email-text">Email</p>
-                        <input type = "text" id = "email" name = "email" placeholder = "Enter your Email"/>
+                        <input type = "text" id = "email" name = "email" placeholder = "Email"/>
                         <p id = "username-text">Username</p>
-                        <input type = "text" id = "username" name = "username" placeholder = "Enter your Username"/>
+                        <input type = "text" id = "username" name = "username" placeholder = "Username"/>
                         <p id = "password-text">Password</p>
-                        <input type = "password" id = "password" name = "password" placeholder= "Enter your password"/>
+                        <input type = "password" id = "password" name = "password" placeholder= "Password"/>
                         <p id = "requirements">Must Contain:
                             <span id = "length">8 Letters, </span>
                             <span id = "numbers">1 Number, </span>
