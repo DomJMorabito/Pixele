40776299849ec89ffd2c1c46702e361496d92b5b
DomJMorabito <domjmorabito@gmail.com>
2025-02-02 13:22:34 -0500

    - Allowed inline scripts again, as they are imperative to functionality.

--- next.config.mjs
+++ next.config.mjs
@@ -34,7 +34,7 @@ const nextConfig = {
                         key: 'Content-Security-Policy',
                         value: [
                             "default-src 'self'",
-                            "script-src 'self'",
+                            "script-src 'self' 'unsafe-inline' 'unsafe-eval' 'strict-dynamic'",
                             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
                             "font-src 'self' https://fonts.gstatic.com https://assets.pixele.gg",
                             "img-src 'self' https://assets.pixele.gg data: blob:",
